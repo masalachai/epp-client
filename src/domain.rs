26d084fa70a22e927fd99ccb1d57200fc526b374
Dirkjan Ochtman <dirkjan@ochtman.nl>
2021-12-09 12:26:22 +0100

    Reinstate domain module as a separate file

---
+++ src/domain.rs
@@ -0,0 +1,11 @@
+pub mod check;
+pub mod create;
+pub mod delete;
+pub mod info;
+pub mod renew;
+pub mod transfer;
+pub mod update;
+
+pub const XMLNS: &str = "urn:ietf:params:xml:ns:domain-1.0";
+
+
