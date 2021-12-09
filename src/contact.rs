95e21eb9eaebb1b5fa344e29f7998d2be4904a26
Dirkjan Ochtman <dirkjan@ochtman.nl>
2021-12-09 12:38:42 +0100

    Reinstate contact module as a separate file

---
+++ src/contact.rs
@@ -0,0 +1,7 @@
+pub mod check;
+pub mod create;
+pub mod delete;
+pub mod info;
+pub mod update;
+
+pub const XMLNS: &str = "urn:ietf:params:xml:ns:contact-1.0";
--- src/lib.rs
+++ src/lib.rs
@@ -99,15 +99,7 @@ pub mod request;
 pub mod response;
 pub mod xml;
 
-pub mod contact {
-    pub mod check;
-    pub mod create;
-    pub mod delete;
-    pub mod info;
-    pub mod update;
-
-    pub const XMLNS: &str = "urn:ietf:params:xml:ns:contact-1.0";
-}
+pub mod contact;
 
 pub mod extensions {
     pub mod consolidate;
